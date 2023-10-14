72ddddfa96d3a055eb0f2f68b185d652116bd4c0
Julio Viveros <vivejulio@outlook.com>
2023-10-14 16:11:45 -0700

    attempting api

--- flask_app/controllers/controller_lists.py
+++ flask_app/controllers/controller_lists.py
@@ -1,6 +1,8 @@
 from flask_app import app
 from flask import render_template, redirect, g, session
 from flask_app.models.model_user import User
+import requests
+from pprint import pprint
 
 @app.route('/dashboard')
 def dashboard():
@@ -10,4 +12,6 @@ def dashboard():
         'id' : session['user_id']
     }
     user = User.get_user_by_id(data)
+    response = requests.get("https://developer.edamam.com//admin/applications/1409623858918")
+    pprint(response.text)
     return render_template('dashboard.html', user = user)
